dc44b983c3f2280f698a86ac31f3388fe2c6c193
Vincent Rouillé <vincent@speedy37.fr>
2019-07-23 23:07:54 +0200

    c bindings massive changes

--- bindings/c/include/update_system.h
+++ bindings/c/include/update_system.h
@@ -1,3 +1,34 @@
+#include <stdint.h>
+
+typedef struct {
+  const char *version;
+  uint8_t update_in_progress;
+} CLocalState;
+
+/// Get current version informations
+/// if `version` is nullptr, use latest version
+extern char* c_local_state(
+  const char* workspace_path,
+  void (*version_callback)(const char *err, const CLocalState* state, void*),
+  void *data
+);
+
+typedef struct {
+  const char *version;
+  const char *description;
+} CRemoteVersion;
+
+/// Get version informations
+/// if `version` is nullptr, use latest version
+extern uint8_t c_version_info(
+  const char* repository_url,
+  const char* username, /* nullable */
+  const char* password, /* nullable */
+  const char* version, /* nullable */
+  void (*version_callback)(const char *err, const CRemoteVersion* info, void*),
+  void *data
+);
+
 typedef struct {
   size_t packages_start;
   size_t packages_end;
@@ -24,21 +55,31 @@ typedef struct {
   double applied_output_bytes_per_sec;
 } CGlobalProgression;
 
-
-extern void c_init_logger(int lvl);
-
-extern void c_free_string(char *ptr);
-extern char* c_update_workspace(
+/// Update workspace to goal_version
+/// if `goal_version` is nullptr, update to latest version
+extern uint8_t c_update_workspace(
   const char* workspace_path,
   const char* repository_url,
-  const char* username,
-  const char* password,
-  const char* goal_version,
-  int (*progress_callback)(const CGlobalProgression*, void*),
+  const char* username, /* nullable */
+  const char* password, /* nullable */
+  const char* goal_version, /* nullable */
+  uint8_t (*progress_callback)(const char *err, const CGlobalProgression* progression, void*),
   void *data
 );
 
-extern char* c_copy_workspace(
+typedef struct {
+  size_t files_start;
+  size_t files_end;
+  uint64_t bytes_start;
+  uint64_t bytes_end;
+
+  size_t failed_files;
+} CCopyProgression;
+
+/// Copy workspace from one directory to another one
+extern uint8_t c_copy_workspace(
   const char* workspace_from,
-  const char* workspace_dest
+  const char* workspace_dest,
+  uint8_t (*progress_callback)(const char *err, const void *CCopyProgression, void*),
+  void *data
 );
--- bindings/c/src/lib.rs
+++ bindings/c/src/lib.rs
@@ -1,18 +1,143 @@
-extern crate env_logger;
-extern crate updater;
-
 use updater::progression::{Progression, TimedProgression};
+use updater::repository::{AutoRepository, RemoteRepository, RepositoryFuture};
+use updater::storage::Versions;
 use updater::update_workspace;
+use updater::updater::Error;
+use updater::workspace::{State, StateUpdating, Workspace};
+
+use futures::{future, future::IntoFuture, Future};
+use log::info;
 
 use std::ffi::CStr;
 use std::ffi::CString;
 use std::ffi::OsStr;
 use std::fs;
 use std::io;
-use std::os::raw::{c_char, c_int, c_void};
+use std::os::raw::{c_char, c_void};
 use std::path::Path;
 use std::ptr;
 use std::time::Duration;
+
+#[repr(C)]
+pub struct CLocalState {
+  pub version: *const c_char,
+  pub update_in_progress: u8,
+}
+
+#[no_mangle]
+pub extern "C" fn c_local_state(
+  workspace_path: *const c_char,
+  version_callback: extern "C" fn(*const c_char, *const CLocalState, *mut c_void),
+  data: *mut c_void,
+) -> u8 {
+  let _ = env_logger::try_init();
+  let workspace_path = unsafe { CStr::from_ptr(workspace_path) }.to_str().unwrap();
+  let mut workspace = Workspace::new(Path::new(workspace_path));
+
+  let res = workspace.load_state().map(|_| workspace.state());
+  match res {
+    Ok(state) => {
+      let (version, update_in_progress) = match state {
+        State::New => (None, 0),
+        State::Stable { version } => (Some(version), 0),
+        State::Updating(StateUpdating { from, .. }) => (Some(from), 0),
+      };
+      let version = version.map(|v| CString::new(v.as_str()).unwrap());
+      version_callback(
+        ptr::null(),
+        &CLocalState {
+          version: version.as_ref().map(|v| v.as_ptr()).unwrap_or(ptr::null()),
+          update_in_progress,
+        },
+        data,
+      );
+      1
+    }
+    Err(err) => {
+      let err = CString::new(format!("{}", err)).unwrap();
+      version_callback(err.as_ptr(), ptr::null(), data);
+      0
+    }
+  }
+}
+
+#[repr(C)]
+pub struct CRemoteVersion {
+  pub version: *const c_char,
+  pub description: *const c_char,
+}
+
+#[no_mangle]
+pub extern "C" fn c_version_info(
+  repository_url: *const c_char,
+  username: *const c_char,
+  password: *const c_char,
+  version: *const c_char,
+  version_callback: extern "C" fn(*const c_char, *const CRemoteVersion, *mut c_void),
+  data: *mut c_void,
+) -> u8 {
+  let _ = env_logger::try_init();
+  let repository_url = unsafe { CStr::from_ptr(repository_url) }.to_str().unwrap();
+  let version = if version.is_null() {
+    None
+  } else {
+    Some(unsafe { CStr::from_ptr(version) }.to_str().unwrap())
+  };
+  let auth = if username.is_null() || password.is_null() {
+    None
+  } else {
+    Some((
+      unsafe { CStr::from_ptr(username) }.to_str().unwrap(),
+      unsafe { CStr::from_ptr(password) }.to_str().unwrap(),
+    ))
+  };
+
+  let res = AutoRepository::new(repository_url, auth)
+    .ok_or(Error::UnsupportedRemote)
+    .and_then(|repository| {
+      let version: RepositoryFuture<String> = if let Some(version) = version {
+        Box::new(future::ok(version.to_owned()))
+      } else {
+        Box::new(repository.current_version().and_then(|c| {
+          info!("latest = {}", c.version());
+          Ok(c.version().to_owned())
+        }))
+      };
+      let versions = repository.versions();
+      (version, versions)
+        .into_future()
+        .map(|(version, versions)| match versions {
+          Versions::V1 { versions } => versions.into_iter().find(|v| v.revision == version),
+        })
+        .map_err(Error::RemoteRepository)
+        .wait()
+    });
+  match res {
+    Ok(Some(version)) => {
+      let revision = CString::new(version.revision.as_str()).unwrap();
+      let description = CString::new(version.description.as_str()).unwrap();
+      version_callback(
+        ptr::null(),
+        &CRemoteVersion {
+          version: revision.as_ptr(),
+          description: description.as_ptr(),
+        },
+        data,
+      );
+      1
+    }
+    Ok(None) => {
+      let err = CString::new("version not found").unwrap();
+      version_callback(err.as_ptr(), ptr::null(), data);
+      0
+    }
+    Err(err) => {
+      let err = CString::new(format!("{}", err)).unwrap();
+      version_callback(err.as_ptr(), ptr::null(), data);
+      0
+    }
+  }
+}
 
 #[repr(C)]
 pub struct CGlobalProgression {
@@ -42,27 +167,16 @@ pub struct CGlobalProgression {
 }
 
 #[no_mangle]
-pub extern "C" fn c_init_logger(_lvl: c_int) {
-  env_logger::init();
-}
-
-#[no_mangle]
-pub extern "C" fn c_free_string(ptr: *mut c_char) {
-  if !ptr.is_null() {
-    let _ = unsafe { CString::from_raw(ptr) };
-  }
-}
-
-#[no_mangle]
 pub extern "C" fn c_update_workspace(
   workspace_path: *const c_char,
   repository_url: *const c_char,
   username: *const c_char,
   password: *const c_char,
   goal_version: *const c_char,
-  progress_callback: extern "C" fn(*const CGlobalProgression, *mut c_void) -> c_int,
+  progress_callback: extern "C" fn(*const c_char, *const CGlobalProgression, *mut c_void) -> u8,
   data: *mut c_void,
-) -> *mut c_char {
+) -> u8 {
+  let _ = env_logger::try_init();
   let workspace_path = unsafe { CStr::from_ptr(workspace_path) }.to_str().unwrap();
   let repository_url = unsafe { CStr::from_ptr(repository_url) }.to_str().unwrap();
   let goal_version = if goal_version.is_null() {
@@ -107,28 +221,45 @@ pub extern "C" fn c_update_workspace(
           applied_input_bytes_per_sec: speed.applied_input_bytes_per_sec(),
           applied_output_bytes_per_sec: speed.applied_output_bytes_per_sec(),
         };
-        progress_callback(&cprogress, data) != 0
+        progress_callback(ptr::null(), &cprogress, data) != 0
       } else {
         true
       }
     },
   );
-  res
-    .map(|_| ptr::null_mut())
-    .unwrap_or_else(|err| CString::new(format!("{}", err)).unwrap().into_raw())
+  if let Err(err) = &res {
+    let err = CString::new(format!("{}", err)).unwrap();
+    progress_callback(err.as_ptr(), ptr::null(), data);
+  }
+  u8::from(res.is_ok())
+}
+
+#[repr(C)]
+pub struct CCopyProgression {
+  pub files_start: usize,
+  pub files_end: usize,
+  pub bytes_start: u64,
+  pub bytes_end: u64,
+  pub failed_files: usize,
 }
 
 #[no_mangle]
 pub extern "C" fn c_copy_workspace(
   workspace_from: *const c_char,
   workspace_dest: *const c_char,
-) -> *mut c_char {
+  progress_callback: extern "C" fn(*const c_char, *const CCopyProgression, *mut c_void) -> u8,
+  data: *mut c_void,
+) -> u8 {
+  let _ = env_logger::try_init();
   let workspace_from = unsafe { CStr::from_ptr(workspace_from) }.to_str().unwrap();
   let workspace_dest = unsafe { CStr::from_ptr(workspace_dest) }.to_str().unwrap();
   let res = copy_dir_recursive(Path::new(workspace_from), Path::new(workspace_dest));
-  res
-    .map(|_| ptr::null_mut())
-    .unwrap_or_else(|err| CString::new(format!("{}", err)).unwrap().into_raw())
+
+  if let Err(err) = &res {
+    let err = CString::new(format!("{}", err)).unwrap();
+    progress_callback(err.as_ptr(), ptr::null(), data);
+  }
+  u8::from(res.is_ok())
 }
 
 fn copy_dir_recursive(from: &Path, to: &Path) -> io::Result<()> {
